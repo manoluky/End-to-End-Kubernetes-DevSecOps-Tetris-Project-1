4aa18d9b63747560cb9a3d9495f1315d702d1a48
manoluky <carrasco.bravo.manuel@gmail.com>
2025-01-14 20:59:56 -0300

    test s3-1

--- EKS-TF/backend.tf
+++ EKS-TF/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "buckettestterraformm"
+    bucket         = "buckettestterraform"
     region         = "us-east-1"
     key            = "End-to-End-Kubernetes-DevSecOps-Tetris-Project/EKS-TF/terraform.tfstate"
     dynamodb_table = "test-lab-tetris"
